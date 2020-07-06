178accb0b1e346c89e360a00af9bd5349d7f749b
Kevin Anderson <kmichael419@hotmail.com>
2020-07-06 13:40:36 -0500

    joke is being input and sent over to server. Once at server, it is pushed into the jokes array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,30 @@
 console.log('client.js sourced');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
+
+const jokeObject = {
+  whoseJoke: null,
+  jokeQuestion: null,
+  punchLine: null,
+};
 
 function onReady() {
-    console.log('DOM ready');
+  console.log('DOM ready');
+  $('#addJokeButton').on('click', sendJoke);
 }
+
+function sendJoke() {
+  jokeObject.whoseJoke = $('#whoseJokeIn').val();
+  jokeObject.jokeQuestion = $('#questionIn').val();
+  jokeObject.punchLine = $('#punchlineIn').val();
+
+  $.ajax({
+    method: 'POST',
+    url: '/api/jokes',
+    data: jokeObject,
+  }).then((response) => {
+    console.log('post joke: ', response);
+  });
+}
+
+function render() {}
